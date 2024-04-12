906c7cb7134d52fa24d6017f40cc8142c8681989
Manvydas <manvis14@gmail.com>
2024-04-12 07:01:19 -0500

    UPD: table_out cte added

--- models/bi_reports/orders_analysis.sql
+++ models/bi_reports/orders_analysis.sql
@@ -43,17 +43,22 @@ orders_aggregate AS (
     SUM(amount) AS amount_sum,
   FROM orders
   GROUP BY customer_id
+),
+
+table_out AS (
+  -- main part
+  SELECT
+    customers_data.title AS customer_title,
+    customers_data.country AS customer_country,
+    orders_aggregate.orders_count,
+    orders_aggregate.amount_sum,
+    -- flag if customer is active
+    orders_count IS NOT NULL AS is_customer_active,
+    CONCAT(customers_data.title, ' from ', customers_data.country, ' country') AS title_with_country,
+  FROM customers_data
+  LEFT JOIN orders_aggregate
+    ON orders_aggregate.customer_id = customers_data.id
 )
 
--- main part
-SELECT
-  customers_data.title AS customer_title,
-  customers_data.country AS customer_country,
-  orders_aggregate.orders_count,
-  orders_aggregate.amount_sum,
-  -- flag if customer is active
-  orders_count IS NOT NULL AS is_customer_active,
-  CONCAT(customers_data.title, ' from ', customers_data.country, ' country') AS title_with_country,
-FROM customers_data
-LEFT JOIN orders_aggregate
-  ON orders_aggregate.customer_id = customers_data.id
+SELECT *
+FROM table_out
